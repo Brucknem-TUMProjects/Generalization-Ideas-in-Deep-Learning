690c3496f3ccc5d2b9b41453f3fb42403013194d
brucknem <mbruckner94@gmail.com>
2019-11-11 00:10:36 +0100

    Spectral and l2 norm working

--- Code/networks/networks.py
+++ Code/networks/networks.py
@@ -1,5 +1,9 @@
+import collections
+import math
+
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.nn.init as init
 
 
 class ExampleNet(nn.Module):
@@ -26,13 +30,169 @@ class ExampleNet(nn.Module):
         return "l2"
 
 
-def extract_per_layer_weights(model):
-    layers = {}
+def extract_layer_names(layer, name="model"):
+    result = {}
 
-    for k, v in model._modules.items():
-        try:
-            layers[k] = model.params[k + '.weight'].cpu().numpy()
-        except:
-            print('Layer "' + k + '" has no weights')
+    if isinstance(layer, nn.Sequential):
+        for i in range(len(layer)):
+            n = str(i)
+            result.setdefault(n, {}).update(**extract_layer_names(layer[i], n))
+    elif isinstance(layer, nn.Module):
+        if not (layer._modules):
+            try:
+                result[name] = layer._parameters['weight'].detach().cpu(
+                ).numpy()
+            except:
+                pass
+                # print(type(layer), " has no weights")
+        else:
+            for k, v in layer._modules.items():
+                n = str(k)
+                result.setdefault(n, {}).update(**extract_layer_names(v, n))
 
-    return layers
+    return flatten(result)
+
+
+def flatten(d, parent_key='', sep='_'):
+    items = []
+
+    for k, v in d.items():
+        new_key = parent_key + sep + k if parent_key else k
+
+        if isinstance(v, collections.MutableMapping):
+            items.extend(flatten(v, new_key, sep=sep).items())
+        else:
+            items.append((new_key, v))
+
+    return dict(items)
+
+
+'''
+Taken from https://github.com/chengyangfu/pytorch-vgg-cifar10
+'''
+
+__all__ = [
+    'VGG',
+    'vgg11',
+    'vgg11_bn',
+    'vgg13',
+    'vgg13_bn',
+    'vgg16',
+    'vgg16_bn',
+    'vgg19_bn',
+    'vgg19',
+]
+
+
+class VGG(nn.Module):
+    '''
+    VGG model
+    '''
+    def __init__(self, features):
+        super(VGG, self).__init__()
+        self.features = features
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(512, 512),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(512, 512),
+            nn.ReLU(True),
+            nn.Linear(512, 10),
+        )
+        # Initialize weights
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                m.bias.data.zero_()
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+
+        return x
+
+
+def make_layers(cfg, batch_norm=False):
+    layers = []
+    in_channels = 3
+
+    for v in cfg:
+        if v == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        else:
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+
+            if batch_norm:
+                layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+            else:
+                layers += [conv2d, nn.ReLU(inplace=True)]
+            in_channels = v
+
+    return nn.Sequential(*layers)
+
+
+cfg = {
+    'A': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'B':
+    [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'D': [
+        64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M',
+        512, 512, 512, 'M'
+    ],
+    'E': [
+        64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512,
+        512, 'M', 512, 512, 512, 512, 'M'
+    ],
+}
+
+
+def vgg11():
+    """VGG 11-layer model (configuration "A")"""
+
+    return VGG(make_layers(cfg['A']))
+
+
+def vgg11_bn():
+    """VGG 11-layer model (configuration "A") with batch normalization"""
+
+    return VGG(make_layers(cfg['A'], batch_norm=True))
+
+
+def vgg13():
+    """VGG 13-layer model (configuration "B")"""
+
+    return VGG(make_layers(cfg['B']))
+
+
+def vgg13_bn():
+    """VGG 13-layer model (configuration "B") with batch normalization"""
+
+    return VGG(make_layers(cfg['B'], batch_norm=True))
+
+
+def vgg16():
+    """VGG 16-layer model (configuration "D")"""
+
+    return VGG(make_layers(cfg['D']))
+
+
+def vgg16_bn():
+    """VGG 16-layer model (configuration "D") with batch normalization"""
+
+    return VGG(make_layers(cfg['D'], batch_norm=True))
+
+
+def vgg19():
+    """VGG 19-layer model (configuration "E")"""
+
+    return VGG(make_layers(cfg['E']))
+
+
+def vgg19_bn():
+    """VGG 19-layer model (configuration 'E') with batch normalization"""
+
+    return VGG(make_layers(cfg['E'], batch_norm=True))
--- Code/norms.py
+++ Code/norms.py
@@ -7,14 +7,25 @@ def margin():
     return 1
 
 
-def l2_norm(layers):
+def lpq_norm(matrix, p, q):
+    if p is np.inf and p is np.inf:
+        return np.max(matrix)
+
+    result = np.absolute(matrix)
+    result = result**p
+    result = result.sum(axis=1)
+    result = result**(q / p)
+    result = result.sum()
+    result = result**(1 / q)
+
+    return result
+
+
+def l2_norm_product(layers):
     result = 1
 
     for layer, weights in layers.items():
-        layer_result = weights**2
-        layer_result = layer_result.sum()
-        layer_result = layer_result**0.5
-        layer_result *= 4
+        layer_result = lpq_norm(weights, 2, 2)
 
         result *= layer_result
 
@@ -29,5 +40,15 @@ def l2_path_norm(weights):
     pass
 
 
-def spectral_norm(weights):
-    pass
+def spectral_norm_product(layers):
+    result = 1
+
+    for layer, weights in layers.items():
+        layer_result = np.matmul(weights, weights.transpose())
+        layer_result = np.linalg.eig(layer_result)[0]
+        layer_result = layer_result.max()
+        layer_result = layer_result**0.5
+
+        result *= layer_result
+
+    return result
